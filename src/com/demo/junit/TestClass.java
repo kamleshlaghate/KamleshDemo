c2334443330033fb47725337cb10cf5b461672b3
Kamlesh Laghate <Kamlesh.Laghate@hansencx.com>
2022-01-24 12:34:46 +0530

    removed spaces

--- src/com/demo/junit/TestClass.java
+++ src/com/demo/junit/TestClass.java
@@ -9,9 +9,6 @@ public class TestClass {
     @Test  
     public void testFindMax(){  
         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
-
-
-        
         assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
     }  
 }
