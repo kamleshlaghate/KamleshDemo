d4185604f24d257b4afcd96800aface7d4cccb9e
Kamlesh Laghate <Kamlesh.Laghate@hansencx.com>
2022-01-24 11:48:03 +0530

    first commit

---
+++ src/com/demo/junit/TestClass.java
@@ -0,0 +1,14 @@
+package com.demo.junit;  
+  
+import static org.junit.Assert.*;  
+import com.demo.test.*;  
+import org.junit.Test;  
+  
+public class TestClass {  
+  
+    @Test  
+    public void testFindMax(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+}
