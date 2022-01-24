e5851d24afb9c7079b78ec66815aa15205f6660d
Kamlesh Laghate <Kamlesh.Laghate@hansencx.com>
2022-01-24 12:18:14 +0530

    added more space

--- src/com/demo/junit/TestClass.java
+++ src/com/demo/junit/TestClass.java
@@ -9,7 +9,8 @@ public class TestClass {
     @Test  
     public void testFindMax(){  
         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
-          
+
+        
         assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
     }  
 }
